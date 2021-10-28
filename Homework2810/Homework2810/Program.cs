61ae725dcc5973f041cf6659cf55c86cd68e6207
sarxanefendi <sarkhanaa@code.edu.az>
2021-10-28 21:31:43 +0400

    Homework 10.28.2021

---
+++ Homework2810/Homework2810/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework2810
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student = new Student("Sarkhan", "Afandizada");
+            student.Mark = 11;
+            Console.WriteLine($"Sarkhan's mark is { student.Mark}");
+            Console.WriteLine(student.ToString());
+
+            string[] menimArrayim = { "Sdsfasda3","Sdsfs", "Saaaas", "Sfasdasffgaagasdgasdfs1", "Sfs", "Sfasdasffgaagasdgasdfs2", "Sfasdasffgaagasdgasdfs3", "Sdsfasda1", "Sdsfasda2", "Sfasdasffgaagasdgasdfs9", };
+
+            Console.WriteLine(Helper.ReturnLongestWord(menimArrayim));
+        }
+    }
+}
---
+++ Homework2810/Homework2810/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2810
+{
+    class Student
+    {
+        public string Name { get; }
+        public string Surname { get; }
+
+        private static int _id = 0;
+
+        public int Id { get; }
+
+        private int mark;
+
+        public int Mark
+        {
+
+            get
+            {
+                return mark;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    mark = value;
+                    Console.WriteLine("Student Mark added");
+                    return;
+                }
+                Console.WriteLine("Please enter correct Mark");
+            }
+        }
+
+        public Student(string name ,string surname)
+        {
+            Name = name;
+            Surname = surname;
+            _id++;
+            Id = _id;
+        }
+
+        public override string ToString()
+        {
+            return $"{Surname} {Name} {Id} {Mark}";
+        }
+    }
+}

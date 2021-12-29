ba6e04b4da5faea60579b6aa81bd6d841505cc7d
sarxanefendi <sarkhanaa@code.edu.az>
2021-12-29 14:47:24 +0400

    ItBrain-Homework

---
+++ ItBrains/ItBrains/Controllers/NewsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItBrains.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
